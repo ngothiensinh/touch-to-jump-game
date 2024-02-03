baf2656e6b110412c03ae9e434aaa999b5529992
Sinh Ngo <ngothiensinh@gmail.com>
2024-02-03 14:51:15 +0700

    Update timing-data.js

--- timing-data.js
+++ timing-data.js
@@ -1,4 +1,4 @@
-// Description: Timing data for the Jumming game
+// Description: Timing data for the Jumping game
 // Rounds is the list of games, each game is the list of step duration in milliseconds)
 let rounds = [
   // round 1: 0~? steps
@@ -10,7 +10,7 @@ let rounds = [
   // round 4: 0~20 steps
   [650, 1070, 1090, 1100, 1150, 1240, 1240, 1250, 1280, 1200,1200, 1300, 1150, 1180, 1200, 1200, 1200, 17, 18, 900],
   // round 5: 0~25 steps
-  [650, 1050, 1500, 1600, 1050, 1600, 1750, 1080, 1550, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
+  [650, 1050, 1500, 1600, 1050, 1600, 1750, 1080, 1550, 1600, 10, 11, 12, 1650, 1100, 1600, 1650, 1680, 18, 19, 20, 21, 22, 23, 24, 25],
   // round 6: 0~? steps
   [650, 1, 2, 3]
 ];
